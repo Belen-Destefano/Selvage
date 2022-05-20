d2b72e4f2691b5bd5f2bc3d5933cd451ade6af35
Belen Destefano <belen.destefano@gmail.com>
2022-05-20 11:32:08 -0300

    primer commit

---
+++ data/categorias.data.js
@@ -0,0 +1,19 @@
+const categorias = [
+    {
+        id:1,
+        nombre:"Ropa",
+        slug: "ropaPortada"
+    }, 
+    {
+        id:2,
+        nombre:"Bikinis",
+        slug: "bikiniPortada"
+    },
+    {
+        id:3,
+        nombre:"Accesorios",
+        slug: "accesoriosPortada"
+    },
+   
+
+]
